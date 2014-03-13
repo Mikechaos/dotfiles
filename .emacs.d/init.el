5e4c44f0912e164b582ac75a85ad049d89547dfb
Mikechaos <mikechaos13@gmail.com>
2014-03-12 20:42:43 -0400

    Cleaned up emacs interface

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
+
 ;; -*- mode: emacs-lisp -*-
 ;; Simple .emacs configuration
 
@@ -6,6 +7,7 @@
 ;; ---------------------
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
+
 (require 'coffee-mode)
 
 (require 'cl)
@@ -30,6 +32,10 @@
 (setq show-trailing-whitespace t)
 (setq suggest-key-bindings nil)
 (setq vc-follow-symlinks t)
+
+
+; add shell mode to bash scripts
+(add-to-list 'auto-mode-alist '("\\.bash\\'" . shell-script-mode))
 
 (global-set-key (kbd "C-h") 'help-command)
 (global-set-key (kbd "M-?") 'mark-paragraph)
@@ -67,17 +73,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(column-marker-1 ((t (:background "red"))) t)
+ '(diff-added ((t (:foreground "cyan"))) t)
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))) t)
  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:background "yellow" :foreground "red")) (((class color) (min-colors 8) (background dark)) (:foreground "red" :weight semi-light))))
- '(fundamental-mode-default ((t (:inherit default))))
+ '(fundamental-mode-default ((t (:inherit default))) t)
  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
  '(linum ((t (:foreground "black" :weight bold))))
  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
+ '(show-paren-match ((((class color) (background light)) (:background "black"))) t)
  '(vertical-border ((t nil))))
 
 ;; ------------
@@ -112,4 +118,86 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (manoj-dark)))
  '(tab-width 4))
+
+
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+
+
+;; CLEAN EMACS
+
+;; Prevent the cursor from blinking
+(blink-cursor-mode 0)
+;; Don't use messages that you don't read
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+;; Don't let Emacs hurt your ears
+(setq visible-bell t)
+
+;; You need to set `inhibit-startup-echo-area-message' from the
+;; customization interface:
+;; M-x customize-variable RET inhibit-startup-echo-area-message RET
+;; then enter your username
+(setq inhibit-startup-echo-area-message "Mikechaos")
+
+;; This is bound to f11 in Emacs 24.4
+;; (toggle-frame-fullscreen) 
+;; Who use the bar to scroll?
+(scroll-bar-mode 0)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+
+;; You can also set the initial frame parameters
+;; (setq initial-frame-alist
+;;       '((menu-bar-lines . 0)
+;;         (tool-bar-lines . 0)))
+
+;; A small minor mode to use a big fringe
+(defvar bzg-big-fringe-mode nil)
+(define-minor-mode bzg-big-fringe-mode
+  "Minor mode to use big fringe in the current buffer."
+  :init-value nil
+  :global t
+  :variable bzg-big-fringe-mode
+  :group 'editing-basics
+  (if (not bzg-big-fringe-mode)
+      (set-fringe-style nil)
+    (set-fringe-mode
+     (/ (- (frame-pixel-width)
+           (* 100 (frame-char-width)))
+        2))))
+
+;; Now activate this global minor mode
+(bzg-big-fringe-mode 1)
+
+;; To activate the fringe by default and deactivate it when windows
+;; are split vertically, uncomment this:
+;; (add-hook 'window-configuration-change-hook
+;;           (lambda ()
+;;             (if (delq nil
+;;                       (let ((fw (frame-width)))
+;;                         (mapcar (lambda(w) (< (window-width w) fw))
+;;                                 (window-list))))
+;;                 (bzg-big-fringe-mode 0)
+;;               (bzg-big-fringe-mode 1))))
+
+;; Use a minimal cursor
+;; (setq cursor-type 'hbar)
+
+;; Get rid of the indicators in the fringe
+(mapcar (lambda(fb) (set-fringe-bitmap-face fb 'org-hide))
+        fringe-bitmaps)
+
+
+;; Set the color of the fringe
+(custom-set-faces
+  '(default ((t (:background "black" :foreground "grey"))))
+  '(fringe ((t (:background "black")))))
+
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
